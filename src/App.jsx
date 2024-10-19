0d7aa5e2e8e5a0b8b0eb197d8831a59be3a18d97
hasib1010 <asifmahmud664@gmail.com>
2024-10-19 12:28:13 +0600

    google analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import './App.css';
-import LandingPage from './Components/LandingPage/LandingPage';
 import Footer from './sharedFile/Footer/Footer';
 import Navbar from './Components/Shared/Nabbar/Navbar';
-import { init, logEvent } from '@react-ga4'; // Import from @react-ga4
+import ReactGA from 'react-ga4';  // Changed to react-ga4
 import React, { useEffect } from 'react';
 
 function App() {
   const location = useLocation();
 
   useEffect(() => {
-    init('G-P35GSSRE75'); // Replace with your Measurement ID
-    logEvent('page_view', { page_path: location.pathname + location.search }); // Track initial page view
-  }, []); // Run only on mount
+    ReactGA.initialize('G-P35GSSRE75');
+  }, []); 
 
   useEffect(() => {
-    logEvent('page_view', { page_path: location.pathname + location.search }); // Track page view on location change
-  }, [location]);
-
-  // Example event tracking function
-  const handleButtonClick = () => {
-    logEvent('button_click', { label: 'Example Button' }); // Track button click event
-  };
+    ReactGA.send({ 
+      hitType: "pageview", 
+      page: location.pathname + location.search 
+    });
+  }, [location]); 
 
   return (
     <div className='flex flex-col justify-between min-h-screen'>
@@ -31,7 +27,6 @@ function App() {
         </div>
         <div className='lg:mx-[70px]'>
           <Outlet />
-          <button onClick={handleButtonClick}>Click Me</button> {/* Example button for event tracking */}
         </div>
       </div>
       <Footer />
