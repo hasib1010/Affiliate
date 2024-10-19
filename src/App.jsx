f8f1e346f83fa397201ab479ff809c6a2dc79ca4
hasib1010 <asifmahmud664@gmail.com>
2024-10-19 12:07:55 +0600

    google analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Footer from './sharedFile/Footer/Footer';
 import Navbar from './Components/Shared/Nabbar/Navbar';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga'; // or import from @react-ga4 for GA4
 import React, { useEffect } from 'react';
 
 function App() {
@@ -11,13 +11,21 @@ function App() {
 
   useEffect(() => {
     ReactGA.initialize('G-P35GSSRE75');
-    ReactGA.pageview(location.pathname + location.search);
-    const unlisten = () => {
-      ReactGA.pageview(location.pathname + location.search);
-    };
+    ReactGA.pageview(location.pathname + location.search); 
+  }, []); // Only run on mount
 
-    return unlisten;
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search); 
   }, [location]);
+
+  // Example event tracking function
+  const handleButtonClick = () => {
+    ReactGA.event({
+      category: 'User',
+      action: 'Clicked Button',
+      label: 'Example Button', // Optional label
+    });
+  };
 
   return (
     <div className='flex flex-col justify-between min-h-screen'>
@@ -27,6 +35,7 @@ function App() {
         </div>
         <div className='lg:mx-[70px]'>
           <Outlet />
+          <button onClick={handleButtonClick}>Click Me</button> 
         </div>
       </div>
       <Footer />
